a14c3fa2dd96bfd80c3647fd0c0c944543d7e652
Raymond Kao <ray@peopleandcode.com>
2016-03-10 12:30:49 -0500

    removed default value for Post.body

--- app/models/post.js
+++ app/models/post.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string', {defaultValue: `Untitled Post ${Date.now()}`}),
-  body: DS.attr('string', {defaultValue: '<section><div class="container"><h2>Hello</h2></div></section><section><div class="container"><h2>World</h2></div></section>'}),
+  body: DS.attr('string'),
   isDraft: DS.attr('boolean', {defaultValue: true})
 });
